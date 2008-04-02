8570eac2871c10ce4125efe9c81acadd862cf8db
loic@call.dachary.org <loic@call.dachary.org>
2008-04-02 10:57:44 +0200

    i18n scripts and packaging

---
+++ mo2json.py
@@ -0,0 +1,38 @@
+#
+# Copyright (c) 2008 Sabin Iacob (m0n5t3r) <iacobs@m0n5t3r.info>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details. 
+#
+import simplejson as enc
+import gettext
+def gettext_json(domain, path, lang = [], indent = False):
+    try:
+        tr = gettext.translation(domain, path, lang)
+        # for unknown reasons, instead of having plural entries like
+        # key: [sg, pl1...]
+        # tr._catalog has (key, n): pln,
+        keys = tr._catalog.keys()
+        keys.sort()
+        ret = {}
+        for k in keys:
+            v = tr._catalog[k]
+            if type(k) is tuple:
+                if k[0] not in ret:
+                    ret[k[0]] = []
+                ret[k[0]].append(v)
+            else:
+                ret[k] = v
+        return enc.dumps(ret, ensure_ascii = False, indent = indent)
+    except IOError:
+        return None
+
+import sys
+print gettext_json(sys.argv[1], '.', [sys.argv[1]]).encode('UTF-8')
+
