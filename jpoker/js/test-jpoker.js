dbbf17d411471cd3cb0711dc79fefaaa0c0eb88c
loic@call.dachary.org <loic@call.dachary.org>
2008-07-08 15:58:12 +0200

    implement jquery frame plugin

--- jpoker/js/test-jpoker.js
+++ jpoker/js/test-jpoker.js
@@ -2293,6 +2293,17 @@ test("profileEnd", function(){
         } catch(e) {}
     });
 
+test("$.fn.frame", function(){
+        expect(4);
+        var element = $("<div id='PID'><div id='ID'></div></div>").appendTo(document.body);
+        $('#ID', element).frame('FRAME');
+        equals($('#PID > .FRAME > .FRAME-s').size(), 1, 'south div');
+        equals($('#PID > .FRAME > #ID').size(), 1, 'ID');
+        $('#PID > .FRAME').trigger('mouseenter');
+        equals($('#PID > .FRAME-hover').size(), 1, 'class FRAME-hover added');
+        $('#PID > .FRAME').trigger('mouseleave');
+        equals($('#PID > .FRAME-hover').size(), 0, 'class FRAME-hover removed');
+    });
 
 //
 // catch leftovers
