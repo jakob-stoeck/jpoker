3b009974e718efd850cabb91313bd8340b5750bd
proppy@pain <proppy@pain>
2008-10-28 17:43:55 +0100

    fix i18n string in js code

--- jpoker/js/jquery.jpoker.js
+++ jpoker/js/jquery.jpoker.js
@@ -2266,7 +2266,7 @@
                         'sit_n_go': _("Sit'n'Go"),
                         'registered': _("Registered")
                         }));
-	var regularPackets = $.grep(packet.packets, function(p, i) {return p.sit_n_go == "n";});
+	var regularPackets = $.grep(packet.packets, function(p, i) {return p.sit_n_go == 'n';});
         for(var i = 0; i < regularPackets.length; i++) {
             var subpacket = regularPackets[i];
             if(!('game_id' in subpacket)) {
@@ -2378,7 +2378,7 @@
                         'sit_n_go': _("Sit'n'Go"),
                         'registered': _("Registered")
                         }));
-	var sitngoPackets = $.grep(packet.packets, function(p, i) {return p.sit_n_go == "y";});
+	var sitngoPackets = $.grep(packet.packets, function(p, i) {return p.sit_n_go == 'y';});
         for(var i = 0; i < sitngoPackets.length; i++) {
             var subpacket = sitngoPackets[i];
             if(!('game_id' in subpacket)) {
@@ -2531,7 +2531,7 @@
 		    'buy_in_label' : _("Buy in:")
                        }).supplant(packet.tourney));
 	
-	if (packet.tourney.state == "registering") {	    
+	if (packet.tourney.state == 'registering') {	    
 	    if (logged) {
 		if (registered) {
 		    html.push(t.register.supplant({'register': _("Unregister")}));
@@ -2541,7 +2541,7 @@
 	    }
 	}
 
-	if (packet.tourney.state == "running" || packet.tourney.state == "complete" || packet.tourney.state == "break" || packet.tourney.state == "breakwait" || packet.tourney.sit_n_go == "y") {
+	if (packet.tourney.state == 'running' || packet.tourney.state == 'complete' || packet.tourney.state == 'break' || packet.tourney.state == 'breakwait' || packet.tourney.sit_n_go == 'y') {
 	    html.push(t.prizes.header.supplant({
                         'caption': _("Prizes"),
 			'rank': _("Rank"),
@@ -2557,7 +2557,7 @@
 	    }			    
 	    html.push(t.prizes.footer);
 	}
-	if (packet.tourney.state == "running" || packet.tourney.state == "break" || packet.tourney.state == "breakwait") {
+	if (packet.tourney.state == "running" || packet.tourney.state == 'break' || packet.tourney.state == 'breakwait') {
 	    html.push(t.tables.header.supplant({
                         'caption': _("Tables"),
 			'table': _("Table"),
@@ -2630,7 +2630,7 @@
 		rows : '<tr><td>{name}</td><td>{money}</td><td>{rank}</td></tr>',
 		footer : '</tbody></table>'
 	    },
-	    "break" : {
+	    'break' : {
 		header : '<table cellspacing=\'0\'><thead><tr class=\'jpoker_thead_caption\'><th colspan=\'3\'>{caption}</th></tr><tr><th>{name}</th><th>{money}</th><th>{rank}</th></tr></thead><tbody>',
 		rows : '<tr><td>{name}</td><td>{money}</td><td>{rank}</td></tr>',
 		footer : '</tbody></table>'
@@ -3264,7 +3264,7 @@
     jpoker.plugins.table.templates = {
         room: 'expected to be overriden by mockup.js but was not',
 	tourney_break: '<div>{label}</div><div>{date}</div>',
-	powered_by: '<a title="Powered by Pokersource" onclick="window.open(this.href); return false" href="http://pokersource.info/"><span>Powered by Pokersource</span></a>',
+	powered_by: '<a title=\'Powered by Pokersource\' onclick=\'window.open(this.href); return false\' href=\'http://pokersource.info/\'><span>Powered by Pokersource</span></a>',
 	chat: '<div class=\'jpoker_chat_input\'><input value=\'chat here\' type=\'text\' width=\'100%\' /></div><div class=\'jpoker_chat_history\'><div class=\'jpoker_chat_history_player\'></div><div class=\'jpoker_chat_history_dealer\'></div></div>'
     };
 
@@ -3501,7 +3501,7 @@
 	},
 
 	avatar: {
-	    template : '<img src="{url}" alt="{name}" />',
+	    template : '<img src=\'{url}\' alt=\'{name}\' />',
 	    update: function(name, url, element) {
                 return element.html(this.template.supplant({ name: name, url: url }));
 	    }
