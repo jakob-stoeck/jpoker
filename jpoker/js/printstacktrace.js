a5256600e97365f1b7e2e3777187e1b580eb8a88
loic@pokerdev.call.tld <loic@pokerdev.call.tld>
2008-11-13 15:30:13 +0100

    printstacktrace coverage

--- jpoker/js/printstacktrace.js
+++ jpoker/js/printstacktrace.js
@@ -5,20 +5,24 @@
 //                  Johan Euphrosine <proppy@aminche.com> (2008)
 // http://pastie.org/253058
 //
-var printStackTrace = function(e, mode) {
-    if(!mode) {
-        mode = this.mode();
-    }
-    if(mode != 'other') {
-        try {(0)()} catch (e) {
-            return this[mode](e);
-        }
-    } else {
-        return this.other();
-    }
+function printStackTrace() {
+    return (new printStackTrace.implementation()).run();
 };
 
-printStackTrace.prototype = {
+printStackTrace.implementation = function() {};
+
+printStackTrace.implementation.prototype = {
+    run: function() {
+        mode = this.mode();
+        if(mode != 'other') {
+            try {(0)()} catch (e) {
+                return this[mode](e);
+            }
+        } else {
+            return this.other(arguments.callee);
+        }
+    },
+
     mode: function() {
         var mode;
         try {(0)()} catch (e) {
@@ -43,7 +47,7 @@ printStackTrace.prototype = {
         for (i=4,j=0,len=lines.length; i<len; i+=2) {
             if (lineRE.test(lines[i])) {
                 lines[j++] = (RegExp.$3 ?
-                              RegExp.$3 + '()@' + RegExp.$2 + RegExp.$1 :
+                              RegExp.$3 + '()@' + RegExp.$2 + ':' + RegExp.$1 :
                               ANON + RegExp.$2 + ':' + RegExp.$1) +
                     ' -- ' + lines[i+1].replace(/^\s+/,'');
             }
@@ -53,36 +57,24 @@ printStackTrace.prototype = {
         return lines;
     },
 
-    other: function() {
-        var curr  = arguments.callee.caller,
-        FUNC  = 'function', ANON = "{anonymous}",
+    other: function(curr) {
+        var ANON = "{anonymous}",
         fnRE  = /function\s*([\w\-$]+)?\s*\(/i,
-        quoteRE = new RegExp('\"', 'g'),
         stack = [],j=0,
-        fn,args,i;
+        fn,args;
+
+        var replacer = function(key, value) {
+            if(typeof this[key] == 'function') {
+                return 'function';
+            } else {
+                return value;
+            }
+        };
 
         while (curr) {
             fn    = fnRE.test(curr.toString()) ? RegExp.$1 || ANON : ANON;
             args  = Array.prototype.slice.call(curr.arguments);
-            i     = args.length;
-
-            while (i--) {
-                switch (typeof args[i]) {
-                case 'string'  : 
-                    args[i] = '"'+args[i].replace(quoteRE,'\\"')+'"'; 
-                    break;
-                case 'function': 
-                    args[i] = FUNC;
-                    break;
-                }
-            }
-
-            if (args && typeof args == 'array') {
-                args = args.join();
-            } else {
-                args = JSON.stringify(args);
-            }
-            stack[j++] = fn + '(' + args + ')';
+            stack[j++] = fn + '(' + JSON.stringify(args, replacer) + ')';
             curr = curr.caller;
         }
 
--- jpoker/js/test-printstacktrace.js
+++ jpoker/js/test-printstacktrace.js
@@ -18,13 +18,53 @@
 
 module("printstacktrace");
 
+test("printStackTrace", function() {
+        expect(1);
+        var r = printStackTrace();
+        equals(typeof r, 'object', 'printStackTrace returns a string');
+    });
+
 test("mode", function() {
         expect(1);
-        equals("firefox other opera".indexOf(printStackTrace.prototype.mode()) >= 0,true);
+        equals("firefox other opera".indexOf(printStackTrace.implementation.prototype.mode()) >= 0,true);
+    });
+
+test("run mode", function() {
+        expect(1);
+        var p = new printStackTrace.implementation();
+        p.other = p.firefox = p.opera = function() { equals(1,1,'called'); }
+        p.run();
+    });
+
+test("run firefox", function() {
+        expect(1);
+        var p = new printStackTrace.implementation();
+        p.mode = function() { return 'firefox'; }
+        p.other = p.opera = function() { equals(1,0,'must not be called'); }
+        p.firefox = function() { equals(1,1,'called'); }
+        p.run();
+    });
+
+test("run opera", function() {
+        expect(1);
+        var p = new printStackTrace.implementation();
+        p.mode = function() { return 'opera'; }
+        p.other = p.firefox = function() { equals(1,0,'must not be called'); }
+        p.opera = function() { equals(1,1,'called'); }
+        p.run();
+    });
+
+test("run other", function() {
+        expect(1);
+        var p = new printStackTrace.implementation();
+        p.mode = function() { return 'other'; }
+        p.opera = p.firefox = function() { equals(1,0,'must not be called'); }
+        p.other = function() { equals(1,1,'called'); }
+        p.run();
     });
 
 test("firefox", function() {
-        var mode = printStackTrace.prototype.mode();
+        var mode = printStackTrace.implementation.prototype.mode();
         var e = [];
         e.push({ stack: 'discarded()...\nf1(1,"abc")@file.js:40\n()@file.js:41\n@:0  \nf44()@file.js:494'});
         if(mode == 'firefox') {
@@ -43,12 +83,73 @@ test("firefox", function() {
         }
         expect(4 * e.length);
         for(var i = 0; i < e.length; i++) {
-            var message = printStackTrace.prototype.firefox(e[i]);
+            var message = printStackTrace.implementation.prototype.firefox(e[i]);
             var message_string = message.join("\n");
             //            equals(message_string, '', 'debug');
             equals(message_string.indexOf('discarded'), -1, 'discarded');
             equals(message[0].indexOf('f1(1,"abc")') >= 0, true, 'f1');
             equals(message[1].indexOf('{anonymous}()') >= 0, true, 'f2 anonymous');
             equals(message[2].indexOf('@:0'), -1, '@:0 discarded');
+        }
+    });
+
+test("opera", function() {
+        var mode = printStackTrace.implementation.prototype.mode();
+        var e = [];
+        e.push({ message: 'discarded\ndiscarded\ndiscarded\ndiscarded\nLine 40 in http://site.com in function f1\n      info f1\nLine 44 in http://site.com\n 	info f2\ndiscarded\ndiscarded'});
+        if(mode == 'opera') {
+            function discarded() {
+                try {(0)()} catch (exception) {
+                    e.push(exception);
+                }
+            };
+            function f1(arg1, arg2) {
+                discarded();
+            };
+            var f2 = function() {
+                f1(1, "abc");
+            };
+            f2();
+        }
+        expect(3 * e.length);
+        for(var i = 0; i < e.length; i++) {
+            var message = printStackTrace.implementation.prototype.opera(e[i]);
+            var message_string = message.join("\n");
+            //equals(message_string, '', 'debug');
+            equals(message_string.indexOf('discarded'), -1, 'discarded');
+            equals(message[0].indexOf('f1()@http://site.com:40 -- info f1') >= 0, true, 'f1');
+            equals(message[1].indexOf('{anonymous}http://site.com:44 -- info f2') >= 0, true, 'f2 anonymous');
+        }
+    });
+
+test("other", function() {
+        var mode = printStackTrace.implementation.prototype.mode();
+        var e = [];
+        var frame = function(args, fun, caller) {
+            this.arguments = args;
+            this.caller = caller;
+            this.fun = fun;
+        };
+        frame.prototype.toString = function() { return JSON.stringify(this); }
+        function f10() {};
+        var frame_f2 = new frame({key: 'no array arg is stringified'}, 'nofunction', undefined);
+        var frame_f1 = new frame([1, 'a"bc', f10], 'FUNCTION f1  (a,b,c)', frame_f2);
+        e.push(frame_f1);
+        if(mode == 'other') {
+            function f1(arg1, arg2) {
+                e.push(arguments.callee);
+            };
+            var f2 = function() {
+                f1(1, 'a"bc', f10);
+            };
+            f2();
+        }
+        expect(2 * e.length);
+        for(var i = 0; i < e.length; i++) {
+            var message = printStackTrace.implementation.prototype.other(e[i]);
+            var message_string = message.join("\n");
+            // equals(message_string, '', 'debug');
+            equals(message[0].indexOf('f1([1,"a\\"bc","function"])') >= 0, true, 'f1');
+            equals(message[1].indexOf('{anonymous}([])') >= 0, true, 'f2 anonymous');
         }
     });
