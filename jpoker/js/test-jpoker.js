542aa35c7f0a764e8fe94ba71ea763f5033aba66
loic@pokerdev.call.tld <loic@pokerdev.call.tld>
2009-01-09 13:28:23 +0100

    release notes and changelog updates

--- jpoker/js/test-jpoker.js
+++ jpoker/js/test-jpoker.js
@@ -1,5 +1,5 @@
 //
-//     Copyright (C) 2008 Loic Dachary <loic@dachary.org>
+//     Copyright (C) 2008, 2009 Loic Dachary <loic@dachary.org>
 //     Copyright (C) 2008 Johan Euphrosine <proppy@aminche.com>
 //
 //     This program is free software: you can redistribute it and/or modify
