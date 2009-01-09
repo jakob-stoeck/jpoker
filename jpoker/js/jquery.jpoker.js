5a798cbb8c2010b6c820c791a2961b653aff8a0c
loic@pokerdev.call.tld <loic@pokerdev.call.tld>
2009-01-09 11:03:13 +0100

    Catch errors in the error handler when trying to display an informative error message and fallback to an obscure error message, which is much better than a stack trace within the error handler that would hide the real problem.

--- jpoker/js/jquery.jpoker.js
+++ jpoker/js/jquery.jpoker.js
@@ -184,22 +184,26 @@
         },
 
         error: function(reason) {
-	    var str;
-            if (reason.xhr) {
-                // We need to give stringify a whitelist so that it doesn't throw an error if it's called on a 
-                // XMLHttpRequest object, and we can't really detect it with instanceof... so let's assume all .xhr
-                // are XMLHttpRequest objects
-                var copy = {};
-                for(key in reason) {
-                    copy[key] = reason[key];
+	    var str = '';
+            try {
+                if (reason.xhr) {
+                    // We need to give stringify a whitelist so that it doesn't throw an error if it's called on a 
+                    // XMLHttpRequest object, and we can't really detect it with instanceof... so let's assume all .xhr
+                    // are XMLHttpRequest objects
+                    var copy = {};
+                    for(key in reason) {
+                        copy[key] = reason[key];
+                    }
+                    copy.xhr = JSON.stringify(copy.xhr, ['status', 'responseText', 'readyState']);
+                    str = JSON.stringify(copy);
+                } else {
+                    str = JSON.stringify(reason);
                 }
-                copy.xhr = JSON.stringify(copy.xhr, ['status', 'responseText', 'readyState']);
-                str = JSON.stringify(copy);
-            } else {
-                str = JSON.stringify(reason);
+                //                str += '\n\n' + printStackTrace({guess:true}).slice(2).join('\n');
+                str += '\n\n' + navigator.userAgent;
+            } catch(e) {
+                str += 'attempt to stringify exception failed';
             }
-            str += '\n\n' + printStackTrace({guess:true}).slice(2).join('\n');
-	    str += '\n\n' + navigator.userAgent;
             this.uninit();
             this.errorHandler(reason, str);
         },
