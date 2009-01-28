28171b465b979562b0262e756e35b160df791a03
Johan Euphrosine <proppy@aminche.com>
2009-01-28 23:41:58 +0000

    add proppy copyright to jpokeradmin

--- jpoker/js/test-jpokeradmin.js
+++ jpoker/js/test-jpokeradmin.js
@@ -1,5 +1,6 @@
 //
 //     Copyright (C) 2009 Loic Dachary <loic@dachary.org>
+//     Copyright (C) 2009 Johan Euphrosine <proppy@aminche.com>
 //
 //     This program is free software: you can redistribute it and/or modify
 //     it under the terms of the GNU General Public License as published by
