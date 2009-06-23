b48af150880da6d73ca56a412841bc4eb9d14dbb
Johan Euphrosine <proppy@aminche.com>
2009-06-23 18:07:32 +0200

    mockupServer in cashier examples

--- jpoker/example/templates/cashier/cashier.js
+++ jpoker/example/templates/cashier/cashier.js
@@ -15,24 +15,19 @@
 //     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-var mockupPacket  = function(options) {	
+var mockupServer  = function(options) {
     var PokerServer = function() {};
+    var server = $.jpoker.url2server('url');
     PokerServer.prototype = {
 	outgoing: "[ " + JSON.stringify(options.packet) + " ]",
 	handle: function(packet) { }
     };
     ActiveXObject.prototype.server = new PokerServer();
-    
-};
-
-var mockupLogin = function(options) {
-    var server = $.jpoker.url2server('url');
     server.serial = options.serial;
 };
 
 var USER_INFO_PACKET = {"rating":1000,"name":"proppy","money":{"X1":[100000,10000,0], "X2":[200000,20000,0]},"affiliate":0,"cookie":"","serial":4,"password":"","type":"PacketPokerUserInfo","email":"","uid__":"jpoker1220102037582"};
-mockupPacket({packet: USER_INFO_PACKET});
-mockupLogin({serial: 42});
+mockupServer({packet: USER_INFO_PACKET, serial: 42});
 
 $(document).ready(function() {	
 	$.jpoker.plugins.cashier.templates.currencies.header = '<ul class=\'jpoker_cashier_currencies\'>';
